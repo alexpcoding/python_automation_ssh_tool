eaa9aebf9d3980e65530e607a8ed8b0cba5adc45
Alex P <somebody@somewhere.net>
2022-03-22 14:42:21 +0000

    refactoring + comments

--- python_ssh_tool.py
+++ python_ssh_tool.py
@@ -14,12 +14,12 @@ def title(text):
 def icmp_check(ip):
     ping = ['ping', ip, '-c', '1']
     print("\nICMP/level 3 testing...", end=' ')
-    p1 = subprocess.Popen(ping, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    if p1.wait() == 0:
+    p = subprocess.Popen(ping, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    if p.wait() == 0:
         return "-->" + '\33[92m' + " OK" + '\033[0m' + "\n"
     else:
         print("-->" + '\33[91m' + " FAILED" + '\033[0m' + "\n")
-        return p1.stdout.read()
+        return p.stdout.read()
 
 
 def port22_check(ip):
@@ -28,29 +28,30 @@ def port22_check(ip):
         print("\nPORT 22 testing...", end=' ')
         if telnetlib.Telnet(ip, port):
             return "-->" + '\33[92m' + " OK" + '\033[0m'
-    except:
-        return "-->" + '\33[91m' + " FAILED" + '\033[0m'
+    except (ConnectionRefusedError, OSError) as e:
+        print("-->" + '\33[91m' + " FAILED" + '\033[0m' + "\n")
+        return e
 
 
 def ssh_check_password(ip, password):
-    global val
+    global val  # boolean variable for ssh connection
     l1 = ['sshpass', '-p', password, 'ssh', '-o', 'StrictHostKeyChecking=no', ip, 'exit']
     print("\nSSH password connection test...", end=' ')
-    p2 = subprocess.Popen(l1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    if p2.wait() == 0:
+    p1 = subprocess.Popen(l1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    if p1.wait() == 0:
         print("-->" + '\33[92m' + " OK" + '\033[0m')
         val = True
     else:
         print("-->" + '\33[91m' + " FAILED" + '\033[0m' + "\n")
-        print(p2.stdout.read())
+        print(p1.stdout.read())
         val = False
 
 
 def ssh_check_key(ip, key):
     global val
-    l1 = ['ssh', '-i', key, '-o', 'StrictHostKeyChecking=no', ip, 'exit']
+    l2 = ['ssh', '-i', key, '-o', 'StrictHostKeyChecking=no', ip, 'exit']
     print("\nSSH key connection test...", end=' ')
-    p2 = subprocess.Popen(l1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    p2 = subprocess.Popen(l2, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
     if p2.wait() == 0:
         print("-->" + '\33[92m' + " OK" + '\033[0m')
         val = True
@@ -60,26 +61,25 @@ def ssh_check_key(ip, key):
         val = False
 
 
+# function will perform connections whether password or key option is chosen
 def ssh_extract(ip):
     print("\n\n###Extracting data...###\n")
     if args.password:
-        l2 = ['sshpass', '-p', pass_input, 'ssh', '-o', 'StrictHostKeyChecking=no', ip, 'hostname;', 'echo "";',
-              'df -h;', 'echo "";',
-              'free -h;', 'echo "";', 'cat /etc/*release;', 'echo "";', 'ip a;', 'echo "";', 'uptime;', 'echo "";',
-              'ss -tuln', '|', ' grep LISTEN']
+        ssh_list = ['sshpass', '-p', pass_input, 'ssh', '-o', 'StrictHostKeyChecking=no', ip, 'hostname;', 'echo "";',
+                    'df -h;', 'echo "";', 'free -h;', 'echo "";', 'cat /etc/*release;', 'echo "";', 'ip a;', 'echo "";',
+                    'uptime;', 'echo "";', 'ss -tuln', '|', ' grep LISTEN']
     else:
-        l2 = ['ssh', '-i', args.key, '-o', 'StrictHostKeyChecking=no', ip, 'hostname;', 'echo "";', 'df -h;',
-              'echo "";',
-              'free -h;', 'echo "";', 'cat /etc/*release;', 'echo "";', 'ip a;', 'echo "";', 'uptime;', 'echo "";',
-              'ss -tuln', '|', ' grep LISTEN']
+        ssh_list = ['ssh', '-i', args.key, '-o', 'StrictHostKeyChecking=no', ip, 'hostname;', 'echo "";', 'df -h;',
+                    'echo "";', 'free -h;', 'echo "";', 'cat /etc/*release;', 'echo "";', 'ip a;', 'echo "";',
+                    'uptime;', 'echo "";', 'ss -tuln', '|', ' grep LISTEN']
 
-    p3 = subprocess.Popen(l2, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    x = p3.stdout.read().split('\n\n')
-    y = ["HOSTNAME", "DISK SPACE", "MEMORY", "DISTRIBUTION DETAILS", "INTERFACES", "UPTIME", "OPEN PORTS"]
+    p3 = subprocess.Popen(ssh_list, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    output_list = p3.stdout.read().split('\n\n')
+    header_list = ["HOSTNAME", "DISK SPACE", "MEMORY", "DISTRIBUTION DETAILS", "INTERFACES", "UPTIME", "OPEN PORTS"]
 
-    for i, j in zip(y, x):
-        title(i)
-        print(j)
+    for header, output in zip(header_list, output_list):
+        title(header)
+        print(output)
 
 
 parser = argparse.ArgumentParser()
@@ -90,14 +90,14 @@ parser.add_argument('-l', '--limit', help="use only one specific host")
 args = parser.parse_args()
 
 with open('hosts.txt') as f:
-    lines = f.read().splitlines()
+    host_list = f.read().splitlines()
 if args.password:
     pass_input = getpass()
 print("")
 
 if args.limit:
-    lines = ["[hosts]", args.limit]
-for host in lines[1:]:
+    host_list = ["[hosts]", args.limit]
+for host in host_list[1:]:
     print('\33[93m' + "==== " + " HOST - " + host + " ====" + '\033[0m')
     print(icmp_check(host), end=' ')
     print(port22_check(host))
@@ -105,5 +105,5 @@ for host in lines[1:]:
         ssh_check_password(host, pass_input)
     else:
         ssh_check_key(host, args.key)
-    if val:
+    if val: # if ssh connection was ok
         ssh_extract(host)
